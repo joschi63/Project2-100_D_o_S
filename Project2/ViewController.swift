80db0dcf25a53eee20d749659e0773d6486c2c45
Johann Schneider <joschi.dev7@gmail.com>
2025-06-28 15:44:25 +0200

    Initial Commit

---
+++ Project2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Johann Schneider on 28.06.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
