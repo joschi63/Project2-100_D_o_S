7890bb320d34871383e0d03d5f58c82348fa1f7d
Johann Schneider <joschi.dev7@gmail.com>
2025-07-05 11:08:27 +0200

    Scorecounter, roundscounter and end alert implemented

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -13,9 +13,16 @@ class ViewController: UIViewController {
     @IBOutlet var button2: UIButton!
     @IBOutlet var button3: UIButton!
     
+    @IBOutlet var scoreText: UILabel!
+    @IBOutlet var numberText: UILabel!
+    
     var countries = [String]()
     var score = 0
     var correctAnswer = 0
+    var answered = 0
+    let maxGames = 10
+    
+    var reset = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,10 +37,14 @@ class ViewController: UIViewController {
         
         countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
         
+        scoreText.text = "Your current score: \(score)"
+        numberText.text = "\(answered) / \(maxGames)"
+        
         askQuestion()
+        
     }
     
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
         countries.shuffle()
         
         button1.setImage(UIImage(named: countries[0]), for: .normal)
@@ -43,8 +54,46 @@ class ViewController: UIViewController {
         correctAnswer = Int.random(in: 0...2)
         
         title = countries[correctAnswer].uppercased()
+
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        var message: String
+        var buttonTitle: String
+        
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong \nThat's the flag of \(countries[sender.tag].uppercased())"
+            score -= 1
+        }
+        answered += 1
+        
+        scoreText.text = "Your current score: \(score)"
+        numberText.text = "\(answered) / \(maxGames)"
+        
+        if answered < maxGames - 1 && !reset{
+            message = ""
+            buttonTitle = "Continue"
+    
+        } else {
+            message = "Game Over! \nYou've played \(answered) games \nYour final score: \(score)"
+            buttonTitle = "Reset"
+            
+            score = 0
+            answered = 0
+        }
+       
+        
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: buttonTitle, style: .default, handler: askQuestion))
+        present(ac, animated: true)
+        
+        
+    }
+    
 }
 
